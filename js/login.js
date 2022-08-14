fae76f265fb270a2b3e64a830958a2cb764fc0f0
mandalestart <MandaleStart@gmail.com>
2022-08-14 15:34:24 -0300

    se agrega validacion de user y pass

--- js/login.js
+++ js/login.js
@@ -1,19 +1,24 @@
 function login() {
     let user = document.getElementById('user').value;
     let pass = document.getElementById('pass').value;
-    let errorText = document.getElementById('errormsg').value;
-    let errorTextLog = document.createTextNode('Usuario y clave son requeridos');
-    
+
+
     if (user != "" && pass != "") {
         localStorage.setItem('user', user);
         location.href = 'index.html';}
 
     else {  
-    
-    errorText.appendChild(errorTextLog);
-    document.getElementById(errormsg).appendChild(errorText);
+        var sp2 = document.getElementById('btnlogin');
+        let errorText = document.createElement('p')
+        let errorTextLog = document.createTextNode('Usuario y clave son requeridos');
+        errorText.id= 'errortextlog'
+        errorText.appendChild(errorTextLog);
+        
+var parentDiv = sp2.parentNode;
+parentDiv.insertBefore(errorText, sp2);
 }
 }
+
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('btnlogin').addEventListener('click', () => { login(); })
 })
