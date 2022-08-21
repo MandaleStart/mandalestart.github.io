8477330e5d0d233fcf8bb13b61eaabf434a93863
mandalestart <MandaleStart@gmail.com>
2022-08-21 09:07:08 -0300

    se corrigen errores en login

--- js/index.js
+++ js/index.js
@@ -1,40 +1,59 @@
 let userlog = localStorage.getItem('user');
+let userdef = localStorage.setItem('userdef', null)
+//                   muestra usuario logeado em nav-bar
+// se crea boton de iniciar sesion
+var logsession = document.createElement('button');
+logsession.id = "logsession"
+logsession.className = "btn btn-success"
+var textLoginButton = document.createTextNode('Iniciar Sesion');
+logsession.appendChild(textLoginButton);
 
+// crea una imagen en el nav-bar
 var userimg = document.createElement('img');
 userimg.src = 'img/img_perfil.png';
-userimg.id ='imgperfil'
+userimg.id = 'imgperfil'
 
+//se crea nombre de usuario en nav-bar
 var username = document.createElement('p');
 username.className = 'username'
 var textUserLog = document.createTextNode(userlog);
 username.appendChild(textUserLog);
 
+// se crea boton de cerrar sesion
 var closesession = document.createElement('button');
-closesession.id="closesession"
-closesession.className="btn btn-danger"
+closesession.id = "closesession"
+closesession.className = "btn btn-danger"
 var textCloseButton = document.createTextNode('Cerrar Sesion');
 closesession.appendChild(textCloseButton);
 
+//                 Control de login en index
 
-
-document.getElementById('sessiondiv').appendChild(userimg);
-document.getElementById('sessiondiv').appendChild(username);
-
-document.getElementById('sessiondiv').appendChild(closesession);
-
-
-function closeS() {
+function closeS() { //cierra la sesion y redirecciona a login
     localStorage.removeItem('user');
     alert('salio de la sesion');
     location.href = 'login.html';
-}
+};
+
+function loginS() {
+    location.href = 'login.html';
+};
+
+function chkS() {
+
+    if (userlog != userdef) {
+        console.log(`hkS:connected user ${userlog}`)
+        document.getElementById('sessiondiv').appendChild(userimg);
+        document.getElementById('sessiondiv').appendChild(username);
+        document.getElementById('sessiondiv').appendChild(closesession);
+    }
+    else {
+        console.log('hkS:unlogged user')
+        document.getElementById('sessiondiv').appendChild(logsession);
+    }
+};
+
 
 document.addEventListener("DOMContentLoaded", function () {
-    function chkS() {
-        if ((localStorage.getItem('user')) = null)
-                {closeS()}
-            else{}};
-
     document.getElementById("autos").addEventListener("click", function () {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
@@ -47,6 +66,7 @@ document.addEventListener("DOMContentLoaded", function () {
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
-    document.getElementById('closesession').addEventListener('click', () => { closeS(); })
-
+    chkS();
+    document.getElementById('closesession')?.addEventListener('click', () => { closeS(); }); // cierre de sesion manual
+    document.getElementById('logsession')?.addEventListener('click', () => { loginS(); }); //redireccion manual al login 
 });
--- js/login.js
+++ js/login.js
@@ -3,26 +3,25 @@ function login() {
     let pass = document.getElementById('pass').value;
     let errorstatus = document.getElementById('errortextlog');
     let numerror = 1
-
+    let nologed = localStorage.getItem('user');
+    
+    //validacion y redireccion 
     if (user != "" && pass != "") {
         localStorage.setItem('user', user);
         location.href = 'index.html';
     }
-
+    //error
     else if (errorstatus < numerror) {
         var sp2 = document.getElementById('btnlogin');
         let errorText = document.createElement('p')
         let errorTextLog = document.createTextNode('Usuario y clave son requeridos');
         errorText.id = 'errortextlog'
         errorText.appendChild(errorTextLog);
-
-        var parentDiv = sp2.parentNode;
-        parentDiv.insertBefore(errorText, sp2);
+        sp2.parentNode.insertBefore(errorText, sp2);
     }
-    else { null }
+    else { null } //  si ya se mostro un mensaje de error no se vuelve a mostrar
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('btnlogin').addEventListener('click', () => { login(); })
+    document.getElementById('btnlogin').addEventListener('click', () => { login(); })  
 })
-*
